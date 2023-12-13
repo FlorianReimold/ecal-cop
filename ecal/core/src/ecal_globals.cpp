d7a8d1fca63c07206132bd9ec8b348be70e1cbc8
tftzee <49162693+rex-schilasky@users.noreply.github.com>
2023-12-13 16:34:46 +0100

    ecal_win_network.h removed

--- ecal/core/src/ecal_globals.cpp
+++ ecal/core/src/ecal_globals.cpp
@@ -22,7 +22,6 @@
 **/
 
 #include "ecal_globals.h"
-#include "ecal_win_network.h"
 
 #include "config/ecal_config_reader.h"
 
@@ -42,11 +41,6 @@ namespace eCAL
   {
     // will be set if any new module was initialized
     bool new_initialization(false);
-
-#ifdef ECAL_OS_WINDOWS
-    // windows network startup
-    Net::Initialize();
-#endif /* ECAL_OS_WINDOWS */
 
     /////////////////////
     // CONFIG
@@ -207,11 +201,6 @@ namespace eCAL
     registration_provider_instance  = nullptr;
     log_instance                    = nullptr;
     config_instance                 = nullptr;
-
-#ifdef ECAL_OS_WINDOWS
-    // windows network cleanup
-    Net::Finalize();
-#endif /* ECAL_OS_WINDOWS */
 
     initialized = false;
 
