96d2068413c69f17b550dbebd779358d7085745d
rex-schilasky <49162693+rex-schilasky@users.noreply.github.com>
2023-12-15 13:07:12 +0100

    segfault in udp sample receiver destruction

--- ecal/core/src/io/udp/ecal_udp_sample_receiver.cpp
+++ ecal/core/src/io/udp/ecal_udp_sample_receiver.cpp
@@ -113,11 +113,11 @@ namespace eCAL
 
     CSampleReceiver::~CSampleReceiver()
     {
-      // destroy udp receiver
-      m_udp_receiver.Destroy();
-
       // stop receiver thread
       m_udp_receiver_thread->stop();
+
+      // destroy udp receiver
+      m_udp_receiver.Destroy();
     }
 
     bool CSampleReceiver::AddMultiCastGroup(const char* ipaddr_)
